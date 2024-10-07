ec0137a9f8db4c76eafe43555381b5a3d720be03
chau-nm <chaunm.146.2001@gmail.com>
2024-10-07 21:26:37 +0700

    oauth2 resource server

---
+++ src/main/java/oauth/resource/server/controller/HelloController.java
@@ -0,0 +1,13 @@
+package oauth.resource.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("hello")
+    public String sayHello() {
+        return "Hello OAuth2 resource server";
+    }
+}
